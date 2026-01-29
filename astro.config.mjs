50485d2e1d6b27ba803b8f4ec4f3894deea11070
Robin Zollner <code@bravegrumpy.com>
2026-01-29 16:26:27 -0500

    some basic additions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import nekoweb from "@indiefellas/astro-adapter-nekoweb"
 
 // https://astro.build/config
 export default defineConfig({
-    adapter: nekoweb({
-        apiKey: process.env.NEKOWEB_API_KEY || '',
-        cookie: `token: ${process.env.NEKOWEB_TOKEN}`,
-        siteName: process.env.SITE_NAME || '',
-        domain: process.env.DOMAIN || ''
-    })
+    output: "static"
 });
