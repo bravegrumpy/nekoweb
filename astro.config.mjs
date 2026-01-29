3a35c68ab3ca4ef21f1686a94d7a607936df9942
Robin Zollner <code@bravegrumpy.com>
2026-01-29 14:41:24 -0500

    stopped using string literals.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,9 @@ import nekoweb from "@indiefellas/astro-adapter-nekoweb"
 // https://astro.build/config
 export default defineConfig({
     adapter: nekoweb({
-        apiKey: "cad9db4c5c8c3993411e5e9d30076702b076d83e41f7e9a5bacce53ddd6c56bc",
-        cookie: "token: 70cb1cbb1556309404fdffed33455fe760e90dd03001616c2d5f00c9133b229b",
-        siteName: "bravegrumpy",
-        domain: "bravegrumpy.nekoweb.org"
+        apiKey: process.env.NEKOWEB_API_KEY || '',
+        cookie: `token: ${process.env.NEKOWEB_TOKEN}`,
+        siteName: process.env.SITE_NAME || '',
+        domain: process.env.DOMAIN || ''
     })
 });
