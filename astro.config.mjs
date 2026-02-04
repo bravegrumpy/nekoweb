07abac15e4905b7e72bbb1da995cb7339912dd36
Robin Zollner <code@bravegrumpy.com>
2026-02-03 21:54:51 -0500

    minor changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    output: "static",
-    // site: `https://${process.env.DOMAIN}`
-    site: "https://bravegrumpy.nekoweb.org"
+  output: "static",
+  
+  // site: `https://${process.env.DOMAIN}`
+  site: "https://bravegrumpy.nekoweb.org",
 });
--- src/pages/posts/post-1.md
+++ src/pages/posts/post-1.md
@@ -1,3 +1,5 @@
+---
+---
 ## Hello
 
 I'm tired, okay?
