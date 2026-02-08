a307a301437781ea9a96a42f9bf4195f2e281723
Robin Zollner <code@bravegrumpy.com>
2026-02-08 15:15:44 -0500

    adding in shadcn and unused ocean theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
@@ -12,5 +14,7 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()],
-  }
+  },
+
+  integrations: [react()]
 });
--- src/pages/posts/post-2.md
+++ src/pages/posts/post-2.md
@@ -1,6 +1,5 @@
 ---
 title: Stuff
-layout: ../layouts/Base.astro
 ---
 
 <div class="flex flex-col gap-5 max-w-sm lg:max-w-4xl w-full mx-auto">
