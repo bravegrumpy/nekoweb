b52eea1129287fe12b91c1ae92d011e9cb3991e3
Robin Zollner <code@bravegrumpy.com>
2026-02-09 13:13:13 -0500

    blank sveltekit again

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ To recreate this project with the same configuration:
 
 ```sh
 # recreate this project
-pnpm dlx sv create --template minimal --types ts --install pnpm ./
+pnpm dlx sv create --template minimal --types ts --add sveltekit-adapter="adapter:static" --install pnpm ./
 ```
 
 ## Developing
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,2 @@
-<script>
-  import { goto } from "$app/navigation";
-</script>
-
-<div class="page">
-  <div class="header">Ocean App</div>
-
-  <div class="card">
-    <h2>Welcome</h2>
-    <p>
-      Drift into a calm ocean-inspired experience. Tap below
-      to play a simple game of Snake.
-    </p>
-
-    <button on:click={() => goto("/play")}>
-      Play Snake
-    </button>
-  </div>
-</div>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
