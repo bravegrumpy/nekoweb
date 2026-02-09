e093a96b130342a682b7bf491ebce02eacd79f3a
Robin Zollner <code@bravegrumpy.com>
2026-02-09 13:10:05 -0500

    I deeply dislike this one

--- svelte.config.js
+++ svelte.config.js
@@ -7,8 +7,8 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			pages: "build",
-			assets: "build",
+			pages: 'build',
+			assets: 'build',
 			fallback: 'not_found.html',
 			precompress: false,
 			strict: true
