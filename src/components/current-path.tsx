3a5f9924f01bb394e41286c725d076c31d2064b8
Robin Zollner <code@bravegrumpy.com>
2026-03-30 13:23:21 -0400

    I like the div better

--- src/components/current-path.tsx
+++ src/components/current-path.tsx
@@ -31,11 +31,11 @@ export function CurrentPath({children}: Props) {
     return (<>
         <p>The page {path ?? "you are looking for" } does not exist.</p>
         { children ?? null }
+        {/*
         <Accordion defaultValue={["item-1"]}>
             <AccordionItem value="item-1">
                 <AccordionTrigger>Would you like to try a different page?</AccordionTrigger>
                 <AccordionContent>
-                    <p></p>
                     <div className="flex flex-row gap-10 justify-between">
                     <Input type="text" value={newPath} onChange={handleChange} />
                     <Link className="bg-secondary text-lg py-1 text-secondary-foreground px-5 w-fit rounded-lg" href={encodeURI(newPath)}>Navigate</Link>
@@ -43,7 +43,7 @@ export function CurrentPath({children}: Props) {
                 </AccordionContent>
             </AccordionItem>
         </Accordion>
-        {/*        
+        */}        
         <div className="flex flex-col gap-3 my-1 ring p-1">
             <p>Would you like to try a different page?</p>
             <div className="flex flex-row gap-10 justify-between">
@@ -51,6 +51,5 @@ export function CurrentPath({children}: Props) {
             <Link href={encodeURI(newPath)} className="bg-secondary text-lg py-1 text-secondary-foreground px-5 w-fit rounded-lg">Navigate</Link>
             </div>
         </div>
-        */}
     </>);
 }
