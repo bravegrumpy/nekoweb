795e9f3a7c2f77fdebce19a7de3df0d6ad09218b
Robin Zollner <code@bravegrumpy.com>
2026-03-30 12:37:40 -0400

    removing "server" type behavior

--- src/components/current-path.tsx
+++ src/components/current-path.tsx
@@ -16,7 +16,8 @@ export function CurrentPath() {
         setNewPath(e.target.value)
     }
     return (<>
-        <p>The page {path ?? "The page you are looking for" } does not exist.</p>
+        {/*<p>The page {path ?? "The page you are looking for" } does not exist.</p>*/}
+        <p>The page you are looking for does not exist.</p>
         <div className="flex flex-col gap-3 my-1 ring p-1">
             <p>Would you like to try a different page?</p>
             <div className="flex flex-row gap-10 justify-between">
