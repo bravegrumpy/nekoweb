c69597d680ed9449722a915896b653deb6060027
Robin Zollner <code@bravegrumpy.com>
2026-04-01 23:18:40 -0400

    creating blank html page for the purposes of making sure the deployment pipeline works.

---
+++ script.js
@@ -0,0 +1,9 @@
+function closeDialog() {
+    const dialog = document.querySelector("dialog");
+    dialog.open = false;
+}
+
+function openDialog() {
+    const dialog = document.querySelector("dialog");
+    dialog.open = true;
+}
--- src/pages/posts/post-1.md
+++
@@ -1,7 +0,0 @@
----
-title: "Hello"
-id: 1
----
-## Hello
-
-I'm tired, okay?
