60150794ff7feecf5a3ccb6e209a7ec90ff465c5
Robin Zollner <code@bravegrumpy.com>
2026-04-04 13:55:08 -0400

    adding some themes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,62 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://bravegrumpy.nekoweb.org",
-    output: 'static'
+  site: "https://bravegrumpy.nekoweb.org",
+  output: 'static',
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Capriola",
+      cssVariable: "--font-capriola"
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "BioRhyme",
+      cssVariable: "--font-bio-rhyme"
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Martian Mono",
+      cssVariable: "--font-martian-mono"
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Inter",
+      cssVariable: "--font-inter"
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Source Serif 4",
+      cssVariable: "--font-source-serif"
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "JetBrains Mono",
+      cssVariable: "--font-jetbrians-mono"
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "DM Sans",
+      cssVariable: "--font-dm-sans"
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Lora",
+      cssVariable: "--font-lora"
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "IBM Plex Mono",
+      cssVariable: "--font-ibm-plex-mono"
+    }
+  ]
 });
---
+++ src/pages/posts/using.md
@@ -0,0 +1,5 @@
+## Current Stack
+
+Right now I am actually using [_AstroJS_](https://astro.build) along with [basecoatui](https://basecoatui.com) and [tailwindcss](https://tailwindcss.com). I don't know exactly how I feel about it, but I do know that it might make more sense to just use "component" class with Tailwind rather than continuing with basecoat.  Basecoat is too close to [ShadCNUI](https://ui.shadcn.com) ad feels pre-creatd. Which is not wrong. But I don't know how I will incorporate custom backgrounds etc.  
+
+It is helpful that the Tailwind config is just CSS these days.
