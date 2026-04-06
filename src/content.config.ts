15e943d9bedc4eee089608aeb052618dd7226345
Robin Zollner <code@bravegrumpy.com>
2026-04-05 20:10:34 -0400

    adding some artwork using content collection

---
+++ src/content.config.ts
@@ -0,0 +1,24 @@
+import { defineCollection } from "astro:content";
+
+import { glob, file } from "astro/loaders"
+
+import { z } from "astro/zod";
+
+const artwork = defineCollection({
+    loader: file("src/data/artworks.json"),
+    schema: ({image}) =>  z.object({
+        id: z.string(),
+        title: z.string(),
+        description: z.string(),
+        image: z.object({
+            src: image(),
+            alt: z.string(),
+            width: z.number(),
+            height: z.number()
+        }),
+        footer: z.string()
+
+    })
+});
+
+export const collections = { artwork }
